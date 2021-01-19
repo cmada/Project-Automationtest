d2c12e436f07d1b775a9cb41794eef467b16a297
cmada <madalina.ciceu@gmail.com>
2021-01-19 22:00:38 +0200

    test - use debugg;assert that; verificari etc

---
+++ src/test/java/search/SimpleSearchTest.java
@@ -0,0 +1,38 @@
+package search;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SimpleSearchTest {
+    @Test
+    public void simpleSearchWithOneKeyWord(){
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        String searchKeyword = "vase";
+
+        driver.findElement(By.id("search")).sendKeys(searchKeyword + Keys.ENTER);
+        System.out.println("Pressed Enter in search field");
+
+        List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name a"));
+
+        System.out.println("Stored " + productNames.size() + " product names");
+
+        for (WebElement productName : productNames){
+
+            assertThat("Some of the products' names do not contain the searched keyword.",
+                    productName.getText(), containsString(searchKeyword.toUpperCase()));
+        }
+    }
+}
