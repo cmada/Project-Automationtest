7297e16c0fe8320870128bc889614b2672df5d19
cmada <madalina.ciceu@gmail.com>
2021-01-20 18:24:32 +0200

    Parameterized tests

--- src/test/java/search/SimpleSearchTest.java
+++ src/test/java/search/SimpleSearchTest.java
@@ -2,23 +2,39 @@ package search;
 
 import org.example.AppConfig;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import webviews.Header;
 import webviews.ProductsGrid;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class SimpleSearchTest extends TestBase {
+
+    private String searchKeyword;
+
+    public SimpleSearchTest(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+    }
+
+    @Parameterized.Parameters
+    public static List<String> data(){
+        return Arrays.asList("vase", "camera");
+    }
+
     @Test
     public void simpleSearchWithOneKeyWord() {
 
         openHomePage();
 
         Header header = PageFactory.initElements(driver, Header.class);
-
-        String searchKeyword = "vase";
 
         header.search(searchKeyword);
 
